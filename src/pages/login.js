deae2b025512174776850c1dcd6ed1ce1a037680
minhthien17300 <minhthien17300@gmail.com>
2021-11-17 20:15:25 +0700

    fetch api login

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,21 +2,45 @@ import React,{useState} from 'react'
 import './login.css'
 import {Link,useHistory} from 'react-router-dom'
 import {useLoginState} from '../State'
+import axios from 'axios';
 function Login() {
 
     const [loginstate,dispatch]=useLoginState()
-    const [user,setUser]=useState({userName:"",userPwd:""});
+    //const [user,setUser]=useState({userName:"",userPwd:""});
     const history=useHistory()
+    const [userName, setUsername] = useState('');
+    const [userPwd, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
     
     
-    const submitHandler=e=>{
+    const submitHandler = async (e) =>{
         e.preventDefault();
-        loginstate.islogin=true;
-        loginstate.Uname=user.userName;
-        console.log(loginstate)
-        history.push('/')
-        
+
+        const response = await fetch('http://localhost:5000/user/login',{
+            //mode:"no-cors",
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            //credentials: 'include',
+            body: JSON.stringify({
+                userName: userName,
+                userPwd: userPwd
+            })
+        })
+
+        if(response.ok)
+        {
+            setRedirect(true);
+            loginstate.islogin=true;
+            loginstate.Uname=userName;
+            var content = await response.json();
+            console.log(content.data)
+        }
+        //history.push('/')
     }
+
+    //if(redirect)
+    //return ('/Home')
+
     return (
         <div className="div-login">
         <form onSubmit={submitHandler}>
@@ -24,11 +48,11 @@ function Login() {
                 <h2>Login</h2>
                 <div className="form-group">
                     <label htmlFor="name">UserName:</label>
-                    <input type="text" name="name" id="name" required onChange={e=>setUser({...user,userName: e.target.value}) } value={user.userName}></input>
+                    <input type="text" name="name" id="name" required onChange={e=>setUsername(e.target.value.toString())} ></input>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" id="password" required onChange={e=>setUser({...user,userPwd: e.target.value}) } value={user.userPwd}></input>
+                    <input type="password" name="password" id="password" required onChange={e=>setPassword(e.target.value.toString()) }></input>
                 </div>
                 
                 <input type="submit" value="Login" ></input>
